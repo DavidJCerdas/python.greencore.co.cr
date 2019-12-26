ca41e74086b362a6c5627a3d3419d133349529a3
djcerdas <d2words@gmail.com>
2019-12-25 23:48:08 -0600

    Add music to this project
    - Adding the feature to play a portion of carol_of_the_bells_mono.wav song

--- HomeWork_1/code.py
+++ HomeWork_1/code.py
@@ -1,12 +1,7 @@
-# CircuitPlaygroundExpress_NeoPixel
-
 import time
 import board
-from neopixel import (
-    NeoPixel
-)
 from digitalio import DigitalInOut, Direction
-
+from adafruit_circuitplayground.express import cpx
 
 RED = (255, 0, 0)
 YELLOW = (200, 150, 25)
@@ -14,28 +9,17 @@ GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
-
-pixels = NeoPixel(board.NEOPIXEL, 10, brightness=0.1)
-
-button_left = DigitalInOut(board.D4)
-button_left.direction = Direction.INPUT
-lstate = button_left.value
-
-button_right = DigitalInOut(board.D5)
-button_right.direction = Direction.INPUT
-rstate = button_right.value
-
-cstate = (lstate,rstate)
-ostate = None
+WHITE = (175, 175, 175)
+BLACK = (0, 0, 0)
 
 
-def recorrer1(pixels, slice = 3):
+def merry_christmas(pixels, slice = 3):
     x = 0
     y = slice
     counter = 0
-    len_pixel = int(len(pixels))
+    len_pixel = int(len(cpx.pixels))
     while counter < len_pixel:
-        pixels[x] = RED
+        cpx.pixels[x] = RED
         time.sleep(0.3)
         if x is not 9:
             pixels[x+1] = CYAN
@@ -45,34 +29,28 @@ def recorrer1(pixels, slice = 3):
             x = y
             y = y + slice
         counter = counter + 1
-    pixels.fill(YELLOW)
+    cpx.play_file("carol_of_the_bells_mono.wav")
     x = 0
     y = slice
     counter = 0
     while counter < len_pixel:
-        pixels[x] = CYAN
+        cpx.pixels[x] = CYAN
         time.sleep(0.3)
         if x is not 9:
-            pixels[x+1] = GREEN
+            cpx.pixels[x+1] = GREEN
             time.sleep(0.3)
-            pixels[x+2] = RED
+            cpx.pixels[x+2] = RED
             time.sleep(0.3)
             x = y
             y = y + slice
         counter = counter + 1
-    pixels.fill(YELLOW)
-
+    cpx.play_file("carol_of_the_bells_mono.wav")
+    cpx.pixels.fill(WHITE)
+    cpx.pixels.fill(BLACK)
+    
 while True:
-    lstate = button_left.value
-    rstate = button_right.value
-    cstate = (lstate, rstate)
-    if cstate != ostate:
-        ostate = cstate
-        if button_left.value:
-            print("Button A Pressed!")
-            recorrer1(pixels, 3)
-        if button_right.value:
-            print("Button B Pressed!")
-            for led in range(int(len(pixels))):
-                pixels[led] = YELLOW
-        pixels.show()
+    lstate = cpx.button_a
+    rstate = cpx.button_b
+    if cpx.button_a or cpx.button_b:
+     print("Button A Pressed!")
+     merry_christmas(cpx.pixels, 3)
