1d0466fafc65df23ed742b3792bee14230d56833
djcerdas <d2words@gmail.com>
2020-01-15 23:07:03 -0600

    Translate to English
    Translate into English to make it easier to read.

--- Practices/print_name.py
+++ Practices/print_name.py
@@ -1,24 +1,24 @@
-name = input("Cual es su nombre?: ")
-
-ERROR1 = "tiene espacios"
-ERROR2 = "solo la primera letra debe ir in mayuscula"
-
+# Simple script to request a name and make some error control
+ERROR1 = "the name has spaces, please put only a name."
+ERROR2 = "only the first letter needs to be in uppercase."
 ERRORS = [0,ERROR1, ERROR2]
 
-def alrevez(palabra):
-    word = list(palabra)
+def alrevez(string_word):
+    word = list(string_word)
     word.reverse()
     return "".join(word)
 
-def control_errores(palabra):
+def control_errores(string_word):
     if " " in name:
         return 1
     if name != name.capitalize():
         return 2
     return 0
 
-imprimo = control_errores(name)
-if imprimo == 0:
-    print("Hola, {0}".format(alrevez(name).capitalize()))
-if imprimo > 0:
-    print("No tiene el formato correcto, {0}".format(ERRORS[imprimo]))
+name = input("Hi, please write your name?: ")
+
+print_x = control_errores(name)
+if print_x == 0:
+    print("Hello, {0}".format(alrevez(name).capitalize()))
+if print_x > 0:
+    print("Error: the name entered does not have the right format, {0}".format(ERRORS[print_x]))
