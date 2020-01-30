ad20791bda2508344525f35b931ab96b53fe7ffb
djcerdas <d2words@gmail.com>
2020-01-30 17:26:54 -0600

    the version for the meetup presentation

---
+++ MeetUp/Python/python_logging.py
@@ -0,0 +1,13 @@
+import logging
+
+# Using Basic Configurations to configure the module:  basicConfig(**kwargs) method
+# logging.basicConfig(level=logging.ERROR, filename="app.log", filemode='a')
+
+# Test the basic configuration.
+# logging.error('This is the first test logging to a file')
+
+# Let's put some format to the messages
+# logging.basicConfig(level=logging.CRITICAL, filename="app.log", filemode='a', format='%(asctime)s: %(name)s - %(levelname)s - %(message)s')
+
+# Test the basic configuration.
+# logging.critical('Testing now a critical message with some format')
