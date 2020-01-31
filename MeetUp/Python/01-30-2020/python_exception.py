ec22773cd9c0a3f2d7ce9b728084709431cb4add
djcerdas <d2words@gmail.com>
2020-01-30 22:45:25 -0600

    file updated to better address the presentation

--- MeetUp/Python/01-30-2020/python_exception.py
+++ MeetUp/Python/01-30-2020/python_exception.py
@@ -1,28 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # This software shows some common exceptions and its handling, this for the next MeetUp
+import os
 
 # Division by 0
 def zero_division_test():
     for i in reversed(range(5)):
         try:
-            print("Lets divide: 10/{0} = {1}".format(i, 10 / i))
+            result = 10 / i
         except ZeroDivisionError:
-            print("ZeroDivisionError catched: Be careful, it is not possible to divide 10 by 0.")
-    print("Done!")
+            print("ZeroDivisionError: Be careful, it is not possible to divide 10 by 0.")
+        else:
+            print("Lets divide: 10/{0} = {1}".format(i, result))
+        finally:
+            print("We are done with {0}".format(i))
+            print("-------------------------------------------------------------------------------")
+    print("This program is done.")
 
-# FileNotFoundError
-def file_not_found_test():
+# raise an exception with specific parameters.
+def raise_customized_e():
     try:
-        f_d = open("dog_names.txt", "r+")
-        print("Printing names:\n"+f_d.read())
-        # Close opened file
-        f_d.close()
-    # Lets get the exception object <exceptions.ZeroDivisionError instance>"
-    except FileNotFoundError as my_exception:
-        print(type(my_exception))
-        print(my_exception)
+        print("# apt install python10")
+        raise Exception(1, 'Python 10 has not yet been released.')
+    except Exception as custom_exception:
+        # type of custom_exception
+        print(type(custom_exception))
+        # Arguments in custom_exception
+        print(custom_exception)
+        # Let's get the arguments back.
+        print("{0}: {1}".format(os.strerror(custom_exception.args[0]), custom_exception.args[1]))
 
 
 # Testing Exceptions
-# zero_division_test()
-# file_not_found_test()
+zero_division_test()
+raise_customized_e()
