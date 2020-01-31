d72dedbb1b3cfdf072f313f550ea7d3f8c99c21f
djcerdas <d2words@gmail.com>
2020-01-30 22:25:49 -0600

    python_logging.py was added to the repository
    Very simple sample code to test logging messages

---
+++ MeetUp/Python/01-30-2020/python_logging.py
@@ -0,0 +1,14 @@
+import logging
+# Very simple sample code to test logging messages 
+
+# Using Basic Configurations to configure the module:  basicConfig(**kwargs) method
+logging.basicConfig(level=logging.ERROR, filename="app.log", filemode='a')
+
+# Test the basic configuration.
+logging.error('This is the first test logging to a file')
+
+# Let's put some format to the messages
+logging.basicConfig(level=logging.CRITICAL, filename="app.log", filemode='a', format='%(asctime)s: %(name)s - %(levelname)s - %(message)s')
+
+# Test the basic configuration.
+logging.critical('Testing now a critical message with some format')
