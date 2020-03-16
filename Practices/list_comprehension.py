90e2abb8d02822303d59bdea5827612f369c2940
djcerdas <d2words@gmail.com>
2020-03-15 20:22:06 -0600

    list comprehension

---
+++ Practices/list_comprehension.py
@@ -0,0 +1,8 @@
+# simple example printing the letters of a word
+word = "Winner"
+my_list = [letter for letter in word]
+print("The letters are: {0}".format(mylist))
+
+# For i in a range 0 to 8, multiply by 2 only the even numbers.
+my_new_list = [i*2 for i in range(0,8) if i%2 == 0]
+print(my_new_list)
